2aa890ae78e890859b8a49bc308ea6004be8cdff
Chus <jvazqu16@institutmvm.cat>
2026-03-21 15:17:46 +0100

    Versió del 18-03-2026

--- cicles/SMX/PROG/solucions/py/05-estructures/tres-en-ratlla-v1.py
+++ cicles/SMX/PROG/solucions/py/05-estructures/tres-en-ratlla-v1.py
@@ -1,3 +1,5 @@
+from colors import valor_amb_color
+
 # En aquesta primera versió només farem dues tasques:
 # - Demanar valors a l'usuari
 # - Pintar la matriu després de cada valor
@@ -18,42 +20,125 @@ def omplir_matriu(matriu):
 
     # Resum: Hem afegit 3 files amb 3 elements a cada fila
 
+
+
 # Funció per imprimir la matriu, tingui el que tingui dins
 # He aprofitat la funció 'imprimir_matriu_3x3_claudators'
 def imprimir_matriu(matriu):
     for fila in range(3):
         fila_a_imprimir = ""
         for columna in range(3):
-            fila_a_imprimir += str(f"[{matriu[fila][columna]}]")
+            valor = matriu[fila][columna]
+            fila_a_imprimir += str(f"[{valor_amb_color(valor)}]")
 
         print(fila_a_imprimir)
 
+def es_numero_valid(valor, min, max):
+    es_valid = True
 
-def obtenir_fila():
-    fila = int(input("Fila: "))
+    if not valor.isnumeric():
+        es_valid = False
+    else:
+        numero = int(valor)
+        if numero < min or numero > max:
+            es_valid = False
+
+    return es_valid
+
+def obtenir_fila(min, max):
+    valor = input(f"Fila[{min}-{max}]: ")            
+    while not es_numero_valid(valor, min, max): # Mentre el valor no sigui un nº o no sigui un nº valid, tornem a preguntar
+        valor = input(f"Fila[{min}-{max}]: ")
+
+    fila = int(valor) # Arribats a aquest punt estem segurs de que es un numero i podem fer int() sense por
     return fila
 
-def obtenir_columna():
-    columna = int(input("Columna: "))
+def obtenir_columna(min, max):
+    columna = int(input(f"Columna[{min}-{max}]: "))
+    while columna < min or columna > max:
+        columna = int(input(f"Columna[{min}-{max}]: "))
     return columna
 
+def es_valor_valid(valor):
+    es_valid = True
+    if valor.startswith("-"):
+        return es_valid
+    else:
+        if valor != "x" and valor !="o" and valor !="stop":
+            es_valid = False
+
+    return es_valid
+
 def obtenir_valor():
-    valor = input("Valor: ")
+    valor = input("Valor [x/o]: ").lower()
+    while not es_valor_valid(valor):
+        valor = input("Valor [x/o]: ").lower()
+
+    if valor.startswith("-"):
+        valor = " "
+
     return valor
 
 def afegir_valor(matriu):
     continuar = True
 
-    fila = obtenir_fila()
-    columna = obtenir_columna()
-    valor = obtenir_valor()
-    
-    if valor == "STOP":
+    fila = obtenir_fila(0,2)
+    columna = obtenir_columna(0,2)
+    valor = obtenir_valor()        
+
+    if valor == "stop":
         continuar = False
     else:
-        matriu[fila][columna] = valor
+        if matriu[fila][columna] == " " or valor == " ": # Si la posició està buida o ens han passat eliminar el valor
+            matriu[fila][columna] = valor # Assignem el valor
+        else: # Si la posició no està buida
+            print(f"La posició[{fila}][{columna}] ja te un valor") # Mostrem error i no fem res, pero permetem continuar 
 
     return continuar
+
+
+def comprovar_files(matriu):
+    guanyador = "" # Per defecte no guanya ningú
+    for fila in matriu:
+        if fila[0] == fila[1] == fila[2] and fila[0] != " ": # Si els tres valors de la fila son iguals
+            guanyador = fila[0] # Retornem el primer valor (encara que donaria igual retornar el segon o tercer)
+
+    return guanyador
+
+def comprovar_columnes(matriu):
+    guanyador = "" # Per defecte no guanya ningú
+    for col in range(3):
+        #Comparem el mateix index de columna a diferents files
+        if matriu[0][col] == matriu[1][col] == matriu[2][col] and matriu[0][col] != " ":
+            guanyador = matriu[0][col]
+
+    return guanyador
+
+def comprovar_diagonals(matriu):
+    guanyador = ""
+    #Comprovem la diagonal principal (de [0][0] a [2][2])
+    if matriu[0][0] == matriu[1][1] == matriu[2][2] and matriu[0][0] != " ":
+        return matriu[0][0]
+
+    #Comprovem la diagonal principal (de [2][0] a [0][2])
+    if matriu[0][2] == matriu[1][1] == matriu[2][0] and matriu[0][2] != " ":
+        return matriu[0][2]
+    return guanyador
+
+def comprovar_guanyador(matriu):
+    guanyador = "" # Per defecte no guanya ningú
+
+    guanyador = comprovar_files(matriu)
+    if guanyador != "": # Si al comprovar les files hem trobat guanyador        
+        return guanyador # El retornem directament, no cal fer més comprovacions
+
+    guanyador = comprovar_columnes(matriu)
+    if guanyador != "": # Si al comprovar les columnes hem trobat guanyador
+        return guanyador # El retornem directament, no cal fer més comprovacions
+    
+    guanyador = comprovar_diagonals(matriu)        
+    # Si revisem tot, retornem guanyador com estigui, tindrà "", "x" o "o"    
+    return guanyador
 
 
 
@@ -66,3 +151,7 @@ continuar = True
 while continuar == True:
     continuar = afegir_valor(matriu)        
     imprimir_matriu(matriu)
+    resultat = comprovar_guanyador(matriu)
+    if resultat != "":
+        print(f"Ha guanyat {resultat}")
+        continuar = False
